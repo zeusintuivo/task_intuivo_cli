17b022fc4c9c57ba52a945e282e7db631e680aa6
zeusintuivo <zeus@intuivo.com>
2021-03-10 08:14:30 +0100

    started adding debian install

--- install_keybase.bash
+++ install_keybase.bash
@@ -4,7 +4,7 @@
 #
 #
   export THISSCRIPTCOMPLETEPATH
-  typeset -gr THISSCRIPTCOMPLETEPATH="$(basename "$0")"   # § This goes in the FATHER-MOTHER script
+  typeset -gr THISSCRIPTCOMPLETEPATH="$(realpath "$0")"   # § This goes in the FATHER-MOTHER script
   export _err
   typeset -i _err=0
 
@@ -16,15 +16,61 @@ load_struct_testing_wget(){
 } # end load_struct_testing_wget
 load_struct_testing_wget
 
-
 export sudo_it
 function sudo_it() {
   raise_to_sudo_and_user_home
 } # end sudo_it
 
-_fedora__64() {
-	sudo_it
-	[ $? -gt 0 ] && failed to sudo_it raise_to_sudo_and_user_home
+_linux_prepare(){
+  sudo_it
+  [ $? -gt 0 ] && failed to sudo_it raise_to_sudo_and_user_home
+  enforce_variable_with_value USER_HOME "${USER_HOME}"
+  local TARGET_URL="${1}"
+  enforce_variable_with_value TARGET_URL "${TARGET_URL}"
+  local CODENAME=$(basename "${TARGET_URL}")
+  enforce_variable_with_value CODENAME "${CODENAME}"
+  local DOWNLOADFOLDER="${USER_HOME}/Downloads"
+  enforce_variable_with_value DOWNLOADFOLDER "${DOWNLOADFOLDER}"
+  _do_not_downloadtwice "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}"
+
+}
+
+_debian__64(){
+  _linux_prepare https://prerelease.keybase.io/keybase_amd64.deb
+  _install_rpm "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}" 0
+} # end __debian__64
+
+_debian__32(){
+  sudo_it
+  [ $? -gt 0 ] && failed to sudo_it raise_to_sudo_and_user_home
+  enforce_variable_with_value USER_HOME "${USER_HOME}"
+  local TARGET_URL=https://prerelease.keybase.io/keybase_i386.deb
+  enforce_variable_with_value TARGET_URL "${TARGET_URL}"
+  local CODENAME=$(basename "${TARGET_URL}")
+  enforce_variable_with_value CODENAME "${CODENAME}"
+  local DOWNLOADFOLDER="${USER_HOME}/Downloads"
+  enforce_variable_with_value DOWNLOADFOLDER "${DOWNLOADFOLDER}"
+  _do_not_downloadtwice "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}"
+  _install_rpm "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}" 0
+} # end __debian__64
+
+_fedora__32() {
+  sudo_it
+  [ $? -gt 0 ] && failed to sudo_it raise_to_sudo_and_user_home
+  enforce_variable_with_value USER_HOME "${USER_HOME}"
+  local TARGET_URL=https://prerelease.keybase.io/keybase_i386.rpm
+  enforce_variable_with_value TARGET_URL "${TARGET_URL}"
+  local CODENAME=$(basename "${TARGET_URL}")
+  enforce_variable_with_value CODENAME "${CODENAME}"
+  local DOWNLOADFOLDER="${USER_HOME}/Downloads"
+  enforce_variable_with_value DOWNLOADFOLDER "${DOWNLOADFOLDER}"
+  _do_not_downloadtwice "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}"
+  _install_rpm "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}" 0
+} # end _fedora__64
+
+_fedora__32() {
+  sudo_it
+  [ $? -gt 0 ] && failed to sudo_it raise_to_sudo_and_user_home
   enforce_variable_with_value USER_HOME "${USER_HOME}"
   local TARGET_URL=https://prerelease.keybase.io/keybase_amd64.rpm
   enforce_variable_with_value TARGET_URL "${TARGET_URL}"
