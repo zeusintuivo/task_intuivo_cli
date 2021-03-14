627dc831a5b4a82ded22abf04f1086d478e0e009
zeusintuivo <zeus@intuivo.com>
2021-03-14 14:45:13 +0100

    added viem and nanoe

--- generate_nginx_reverse_proxy_template.conf.sh
+++ generate_nginx_reverse_proxy_template.conf.sh
@@ -7,6 +7,79 @@ PROJECTNAME=$(basename $(pwd))
 SERVERNAME=${PROJECTNAME}.test
 CERTIFICATECRTPATH=${HOME}/.valet/Certificates/${SERVERNAME}.crt;
 CERTIFICATEKEYPATH=${HOME}/.valet/Certificates/${SERVERNAME}.key;
+
+
+STATICFILES=""
+
+
+.dirs() {
+    find * -maxdepth 0 -type d   # mac and linux tested
+}
+# DEBUG=1
+# REMOVECACHE=1
+.subdir() {
+  # Perform all actions in
+  #        LIST1
+  #          for each element in
+  #                           LIST2
+
+  local _curdir="${1}"
+  local _subdir=""
+  local local_actions="${2}"
+  local local_items="$(.dirs)"
+  local one_item
+  local one_action
+  local action
+  # local _subdirs
+  local _actions
+  cd "${_curdir}"
+  local _cwd="$(pwd)"
+
+  while read -r one_item; do
+  {
+    if [ ! -z "${one_item}" ] ; then  # if not empty
+    {
+      action="${one_actions/\{\#\}/$one_item}"  # replace value inside string substitution expresion bash
+      echo ${action}
+        _subdir="${_cwd}/${one_item}"
+      cd "${_subdir}"
+      pwd
+      [[ -d .git/ ]] && pull
+      if [[ ! -d .git/ ]] ; then
+      {
+        _actions="echo -e \".\"
+        echo \" \"
+        echo \"Subdirectory:\"
+        echo \". \"
+        echo \". . . . . . .       {#} \"
+        cd \"${_subdir}/{#}\"
+        pwd
+        [[ -d .git/ ]] && pull
+        "
+        .subdir "${_subdir}" "${_actions}"
+      }
+      fi
+    }
+    fi
+  }
+  done <<< "${local_items}"
+}
+
+
+CURDIR="${PWD}"
+ACTIONS="echo -e \".\"
+echo \" \"
+echo \"Directory:\"
+echo \". \"
+echo \". . . . . . .       {#} \"
+cd \"${CURDIR}/{#}\"
+pwd
+[[ -d .git/ ]] && pull
+"
+
+.subdir  "${CURDIR}" "${ACTIONS}" 
+
+
 echo "upstream ${PROJECTNAME} {
     # Path to Puma SOCK file, as defined previously
     # NodeJS Express Etc ANything with custom port localhost:8080 localhost:3000 etc
