7dbcdda9f981b2a359cd67d1868ace317fb651ff
zeusintuivo <zeus@intuivo.com>
2021-05-30 18:25:57 +0200

    removed add_error_trap.sh readonly from USER_HOME var in typeset

--- install_discord.bash
+++ install_discord.bash
@@ -1,145 +1,257 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # @author Zeus Intuivo <zeus@intuivo.com>
 #
-#
-  export THISSCRIPTCOMPLETEPATH
-  typeset -gr THISSCRIPTCOMPLETEPATH="$(basename "$0")"   # § This goes in the FATHER-MOTHER script
-  export _err
-  typeset -i _err=0
+# 20200415 Compatible with Fedora, Mac, Ubuntu "sudo_up" "load_struct"
+set -E -o functrace
+export THISSCRIPTCOMPLETEPATH
+typeset -r THISSCRIPTCOMPLETEPATH="$(realpath  "$0")"
+export BASH_VERSION_NUMBER
+typeset BASH_VERSION_NUMBER=$(echo $BASH_VERSION | cut -f1 -d.)
 
-function raise_to_sudo_and_user_home() {
-  # export  THISSCRIPTCOMPLETEPATH
-  # typeset -gr THISSCRIPTCOMPLETEPATH="$(basename "$0")"
-  # export _err
-  # typeset -i _err=0
-  load_execute_boot_basic_with_sudo(){
-    # shellcheck disable=SC2030
-    if ( typeset -p "SUDO_USER"  &>/dev/null ) ; then
-    {
-      export USER_HOME
-      # typeset -rg USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)  # Get the caller's of sudo home dir Just Linux
-      # shellcheck disable=SC2046
-      # shellcheck disable=SC2031
-      typeset -rg USER_HOME="$(echo -n $(bash -c "cd ~${SUDO_USER} && pwd"))"  # Get the caller's of sudo home dir LINUX and MAC
-    }
-    else
-    {
-      local USER_HOME=$HOME
-    }
-    fi
-    local -r provider="$USER_HOME/_/clis/execute_command_intuivo_cli/execute_boot_basic.sh"
-    echo source "${provider}"
-    # shellcheck disable=SC1090
-    [   -e "${provider}"  ] && source "${provider}"
-    [ ! -e "${provider}"  ] && eval """$(wget --quiet --no-check-certificate  https://raw.githubusercontent.com/zeusintuivo/execute_command_intuivo_cli/master/execute_boot_basic.sh -O -  2>/dev/null )"""   # suppress only wget download messages, but keep wget output for variable
-    if ( command -v failed >/dev/null 2>&1; ) ; then
-    {
-      return 0
-    }
-    else
-    {
-      echo -e "\n \n  ERROR! Loading execute_boot_basic.sh \n \n "
-      exit 1;
-    }
-    fi
-    return 0
-  } # end load_execute_boot_basic_with_sudo
+export  THISSCRIPTNAME
+typeset -r THISSCRIPTNAME="$(basename "$0")"
 
-  load_execute_boot_basic_with_sudo
+export _err
+typeset -i _err=0
+  function _trap_on_error(){
+    echo -e "\\n \033[01;7m*** ERROR TRAP $THISSCRIPTNAME \\n${BASH_SOURCE}:${BASH_LINENO[-0]} ${FUNCNAME[-0]}() \\n$0:${BASH_LINENO[1]} ${FUNCNAME[1]}() \\n ERR ...\033[0m"
+    exit 1
+  }
+  trap _trap_on_error ERR
+  function _trap_on_int(){
+    echo -e "\\n \033[01;7m*** INTERRUPT TRAP $THISSCRIPTNAME \\n${BASH_SOURCE}:${BASH_LINENO[-0]} ${FUNCNAME[-0]}() \\n$0:${BASH_LINENO[1]} ${FUNCNAME[1]}() \\n  INT ...\033[0m"
+    exit 0
+  }
+
+  trap _trap_on_int INT
+
+load_struct_testing(){
+  function _trap_on_error(){
+    local -ir __trapped_error_exit_num="${2:-0}"
+    echo -e "\\n \033[01;7m*** ERROR TRAP $THISSCRIPTNAME \\n${BASH_SOURCE}:${BASH_LINENO[-0]} ${FUNCNAME[-0]}() \\n$0:${BASH_LINENO[1]} ${FUNCNAME[1]}() \\n ERR ...\033[0m  \n \n "
+    echo ". ${1}"
+    echo ". exit  ${__trapped_error_exit_num}  "
+    echo ". caller $(caller) "
+    echo ". ${BASH_COMMAND}"
+    local -r __caller=$(caller)
+    local -ir __caller_line=$(echo "${__caller}" | cut -d' ' -f1)
+    local -r __caller_script_name=$(echo "${__caller}" | cut -d' ' -f2)
+    awk 'NR>L-10 && NR<L+10 { printf "%-10d%10s%s\n",NR,(NR==L?"☠ » » » > ":""),$0 }' L="${__caller_line}" "${__caller_script_name}"
+
+    # $(eval ${BASH_COMMAND}  2>&1; )
+    # echo -e " ☠ ${LIGHTPINK} Offending message:  ${__bash_error} ${RESET}"  >&2
+    exit 1
+  }
+  trap  '_trap_on_error $0 "${?}" LINENO BASH_LINENO FUNCNAME BASH_COMMAND $FUNCNAME $BASH_LINENO $LINENO   $BASH_COMMAND'  ERR
+    local provider="$HOME/_/clis/execute_command_intuivo_cli/struct_testing"
+    local _err=0 structsource
+    if [   -e "${provider}"  ] ; then
+      echo "Loading locally"
+      structsource="""$(<"${provider}")"""
+      _err=$?
+      [ $_err -gt 0 ] &&  echo -e "\n \n  ERROR! Loading struct_testing. running 'source locally' returned error did not download or is empty err:$_err  \n \n  " && exit 1
+    else
+      if ( command -v curl >/dev/null 2>&1; )  ; then
+        echo "Loading struct_testing from the net using curl "
+        structsource="""$(curl https://raw.githubusercontent.com/zeusintuivo/execute_command_intuivo_cli/master/struct_testing  -so -   2>/dev/null )"""  #  2>/dev/null suppress only curl download messages, but keep curl output for variable
+        _err=$?
+        [ $_err -gt 0 ] &&  echo -e "\n \n  ERROR! Loading struct_testing. running 'curl' returned error did not download or is empty err:$_err  \n \n  " && exit 1
+      elif ( command -v wget >/dev/null 2>&1; ) ; then
+        echo "Loading struct_testing from the net using wget "
+        structsource="""$(wget --quiet --no-check-certificate  https://raw.githubusercontent.com/zeusintuivo/execute_command_intuivo_cli/master/struct_testing -O -   2>/dev/null )"""  #  2>/dev/null suppress only wget download messages, but keep wget output for variable
+        _err=$?
+        [ $_err -gt 0 ] &&  echo -e "\n \n  ERROR! Loading struct_testing. running 'wget' returned error did not download or is empty err:$_err  \n \n  " && exit 1
+      else
+        echo -e "\n \n  ERROR! Loading struct_testing could not find wget or curl to download  \n \n "
+        exit 69
+      fi
+    fi
+    [[ -z "${structsource}" ]] && echo -e "\n \n  ERROR! Loading struct_testing. structsource did not download or is empty " && exit 1
+    local _temp_dir="$(mktemp -d 2>/dev/null || mktemp -d -t 'struct_testing_source')"
+    echo "${structsource}">"${_temp_dir}/struct_testing"
+    echo "Temp location ${_temp_dir}/struct_testing"
+    source "${_temp_dir}/struct_testing"
+    _err=$?
+    [ $_err -gt 0 ] &&  echo -e "\n \n  ERROR! Loading struct_testing. Occured while running 'source' err:$_err  \n \n  " && exit 1
+    if  ! typeset -f passed >/dev/null 2>&1; then
+      echo -e "\n \n  ERROR! Loading struct_testing. Passed was not loaded !!!  \n \n "
+      exit 69;
+    fi
+    return $_err
+} # end load_struct_testing
+load_struct_testing
+
+ _err=$?
+[ $_err -ne 0 ]  && echo -e "\n \n  ERROR FATAL! load_struct_testing_wget !!! returned:<$_err> \n \n  " && exit 69;
+
+export sudo_it
+function sudo_it() {
+  raise_to_sudo_and_user_home
+  [ $? -gt 0 ] && failed to sudo_it raise_to_sudo_and_user_home && exit 1
+  enforce_variable_with_value SUDO_USER "${SUDO_USER}"
+  enforce_variable_with_value SUDO_UID "${SUDO_UID}"
+  enforce_variable_with_value SUDO_COMMAND "${SUDO_COMMAND}"
+  # Override bigger error trap  with local
+  function _trap_on_error(){
+    echo -e "\033[01;7m*** TRAP $THISSCRIPTNAME \\n${BASH_SOURCE}:${BASH_LINENO[-0]} ${FUNCNAME[-0]}() \\n$0:${BASH_LINENO[1]} ${FUNCNAME[1]}() \\n ERR INT ...\033[0m"
+  }
+  trap _trap_on_error ERR INT
+} # end sudo_it
+
+# _linux_prepare(){
+  sudo_it
+  [ $? -gt 0 ] && (failed to sudo_it raise_to_sudo_and_user_home  || exit 1)
+  export USER_HOME
+  # shellcheck disable=SC2046
+  # shellcheck disable=SC2031
+  typeset -r USER_HOME="$(echo -n $(bash -c "cd ~${SUDO_USER} && pwd"))"  # Get the caller's of sudo home dir LINUX and MAC
+  # USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)   # Get the caller's of sudo home dir LINUX
+  enforce_variable_with_value USER_HOME "${USER_HOME}"
+# }  # end _linux_prepare
+
+
+# _linux_prepare
+
+enforce_variable_with_value USER_HOME $USER_HOME
+enforce_variable_with_value SUDO_USER $SUDO_USER
+passed Caller user identified:$SUDO_USER
+passed Home identified:$USER_HOME
+directory_exists_with_spaces "$USER_HOME"
+
+
+
+ #--------\/\/\/\/-- install_discord.bash -- Custom code -\/\/\/\/-------
+
+
+function only_alphanumeric(){
+ sed 's/[^a-zA-Z0-9]//g'
+}
+function only_url_chars(){
+ sed 's/[^a-zA-Z0-9_:.\/-]//g'
+}
+function just_download_it(){
+  # Sample use
+  #
+  #   _just_download_it
+  local TARGET_URL=$(echo ${1} | only_url_chars)
+  enforce_variable_with_value TARGET_URL "${TARGET_URL}"
+  local CODENAME=$(echo ${2} | only_url_chars)
+  enforce_variable_with_value CODENAME "${CODENAME}"
+  echo  "TARGET_URL:<${TARGET_URL}>"
+  echo  "  CODENAME:<${CODENAME}>"
+  if ( command -v curl >/dev/null 2>&1; )  ; then
+    # echo "Downloading ${TARGET_URL} from the net using curl "
+    # REF: https://www.digitalocean.com/community/tutorials/workflow-downloading-files-curl
+    echo curl -LO "${TARGET_URL}"
+    #echo curl -L -o "${CODENAME}"  "${TARGET_URL}"
+    #curl -L -o "${CODENAME}"  "${TARGET_URL}"
+    (( DEBUG )) && echo "https://dl.discordapp.net/apps/linux/0.0.15/discord-0.0.15.tar.gz" | hexdump -C
+    (( DEBUG )) && echo ${TARGET_URL} |  hexdump -C
+    curl -LO ${TARGET_URL}
+    _err=$?
+    [ $_err -gt 0 ] &&  echo -e "\n \n  ERROR! Downloading \n\n ${TARGET_URL}. running 'curl' returned error did not download or is empty err:$_err  \n \n  " && exit 1
+    return ${_err}
+  elif ( command -v wget >/dev/null 2>&1; ) ; then
+    echo "Downloading ${TARGET_URL} from the net using wget "
+    # REF: https://linuxconfig.org/wget-file-download-on-linux
+    wget --quiet --no-check-certificate  "${TARGET_URL}" -O "${CODENAME}"
+    _err=$?
+    [ $_err -gt 0 ] &&  echo -e "\n \n  ERROR! Downloading ${TARGET_URL}. running 'wget' returned error did not download or is empty err:$_err  \n \n  " && exit 1
+    return ${_err}
+  else
+    echo -e "\n \n  ERROR! Downloading ${TARGET_URL}  could not find wget or curl to download  \n \n "
+    exit 69
+  fi
+  return 1
+} # end _just_download_it
+
+_fedora__64() {
+  raise_to_sudo_and_user_home
+  [ $? -gt 0 ] && failed to raise_to_sudo_and_user_home
+  # [ $? -gt 0 ] && exit 1
+  enforce_variable_with_value USER_HOME "${USER_HOME}"
+  local FIRST_URL="https://discord.com/api/download?platform=linux&format=tar.gz"
+  enforce_variable_with_value FIRST_URL "${FIRST_URL}"
+  local CURLOUTPUT="$(curl -IL "${FIRST_URL}" 2>/dev/null)"
+  echo "curl -IL \"${FIRST_URL}\"  | grep \"location:\" | cut -d: -f2-"
+  # local CURLOUTPUT="$(curl -IL https://discord.com/api/download\?platform\=linux\&format\=tar.gz 2>/dev/null)"
+  enforce_variable_with_value CURLOUTPUT "${CURLOUTPUT}"
+  local TARGET_URL=$(echo "${CURLOUTPUT}" | grep "location:" | cut -d: -f2- | only_url_chars)
+  enforce_variable_with_value TARGET_URL "${TARGET_URL}"
+  local CODENAME=$(basename "${TARGET_URL}" | only_url_chars)
+  enforce_variable_with_value CODENAME "${CODENAME}"
+  local DOWNLOADFOLDER="$(_find_downloads_folder)"
+  enforce_variable_with_value DOWNLOADFOLDER "${DOWNLOADFOLDER}"
+  cd "${DOWNLOADFOLDER}"
+  local _err=0
+  just_download_it "${TARGET_URL}"  "${CODENAME}"
   _err=$?
   if [ $_err -ne 0 ] ;  then
   {
-    >&2 echo -e "ERROR There was an error loading load_execute_boot_basic_with_sudo Err:$_err "
+    >&2 echo -e "${RED} ✘ ${YELLOW_OVER_DARKBLUE}${*}${RESET} Err:$_err Message: error deleting directory"
     exit $_err
   }
   fi
-
-  # Overwrite trap
-  # function _trap_on_exit(){
-    # echo -e "\033[01;7m*** TRAP $THISSCRIPTCOMPLETEPATH EXITS ...\033[0m"
-  # }
-  #trap kill ERR
-  # trap _trap_on_exit EXIT
-  #trap kill INT
-} # end raise_to_sudo_and_user_home
-load_struct_testing_wget(){
-    local provider="$HOME/_/clis/execute_command_intuivo_cli/struct_testing"
-    [   -e "${provider}"  ] && source "${provider}" && echo "Loaded locally"
-    [ ! -e "${provider}"  ] && eval """$(wget --quiet --no-check-certificate  https://raw.githubusercontent.com/zeusintuivo/execute_command_intuivo_cli/master/struct_testing -O -  2>/dev/null )"""   # suppress only wget download messages, but keep wget output for variable
-    ( ( ! command -v passed >/dev/null 2>&1; ) && echo -e "\n \n  ERROR! Loading struct_testing \n \n " && exit 69; )
-} # end load_struct_testing_wget
-load_struct_testing_wget
-
-
-_downloadfile_link(){
-	_downloadfile_link "${PLATFORM}"  "${CODELASTESTBUILD}" "${URL}"
-	local CODENAME=""
-  case ${1} in
-  mac)
-    CODENAME="${3}-${2}.dmg"
-    ;;
-
-  windows)
-    CODENAME="${3}-${2}.exe"
-    CODENAME="${3}-${2}.win.zip"
-    ;;
-
-  linux)
-    CODENAME="${3}-${2}.tar.gz"
-    ;;
-
-  *)
-    CODENAME=""
-    ;;
-  esac
-  echo "${CODENAME}"
+  file_exists_with_spaces "${DOWNLOADFOLDER}/${CODENAME}"
+  tar -xvf "${DOWNLOADFOLDER}/${CODENAME}"
+  file_exists_with_spaces "${DOWNLOADFOLDER}/Discord"
+  directory_exists_with_spaces "/usr/lib64"
+  directory_exists_with_spaces "/usr/bin"
+  if  it_exists_with_spaces "/usr/lib64/discord" ; then
+  {
+    rm -rf "/usr/lib64/discord"
+    _err=$?
+  }
+  fi
+  if [ $_err -ne 0 ] ;  then
+  {
+    >&2 echo -e "${RED} ✘ ${YELLOW_OVER_DARKBLUE}${*}${RESET} Err:$_err Message: error deleting directory"
+    exit $_err
+  }
+  fi
+  directory_does_not_exist_with_spaces "/usr/lib64/discord"
+  if ! it_exists_with_spaces "/usr/bin/Discord" ; then
+  {
+    unlink "/usr/bin/Discord"
+    _err=$?
+  } else {
+    echo "whot"
+  }
+  fi
+  if [[ -L "/usr/bin/Discord" ]] ; then
+  {
+    echo "what"
+    unlink "/usr/bin/Discord"
+    _err=$?
+  }
+  fi
+  if [ $_err -ne 0 ] ;  then
+  {
+    >&2 echo -e "${RED} ✘ ${YELLOW_OVER_DARKBLUE}${*}${RESET} Err:$_err Message: error unlinking /usr/lib64/discord"
+    exit $_err
+  }
+  fi
+  file_does_not_exist_with_spaces "/usr/bin/Discord"
+  mv "${DOWNLOADFOLDER}/Discord" "/usr/lib64/discord"
+   _err=$?
+  if [ $_err -ne 0 ] ;  then
+  {
+    >&2 echo -e "${RED} ✘ ${YELLOW_OVER_DARKBLUE}${*}${RESET} Err:$_err Message: error moving directory from "${DOWNLOADFOLDER}/Discord" to /usr/lib64/discord"
+    exit $_err
+  }
+  fi
+  directory_exists_with_spaces "/usr/lib64/discord"
+  cd /usr/bin
+  ln -s /usr/lib64/discord/Discord /usr/bin/Discord
+  softlink_exists_with_spaces "/usr/bin/Discord>/usr/lib64/discord/Discord"
   return 0
-} # end _downloadfile_link
-
-_version() {
-  local URL="${1}"      # https://www.website.com/product/
-  #                    param order varname    varvalue    sampleregex
-  enforce_parameter_with_value 1 URL "${URL}" "https://www.website.com/product/"
-  local PLATFORM="${2}" # mac windows linux
-  #                    param order varname    varvalue      validoptions
-  enforce_parameter_with_options 2 PLATFORM "${PLATFORM}" "mac windows linux"
-  local PATTERN="${3}"
-  #                    param order varname    varvalue    sampleregex
-  enforce_parameter_with_value 3 PATTERN "${PATTERN}" "BCompareOSX*.*.*.*.zip"
-  DEBUG=1
-  local CODEFILE="""$(wget --quiet --no-check-certificate  "${URL}" -O -  2>/dev/null )""" # suppress only wget download messages, but keep wget output for variable
-  enforce_variable_with_value CODEFILE "${CODEFILE}"
-  local CODELASTESTBUILD=$(_extract_version "${CODEFILE}")
-  enforce_variable_with_value CODELASTESTBUILD "${CODELASTESTBUILD}"
-  echo "${CODELASTESTBUILD}"
-  return 0
-} # end _version
-
-_extract_version(){
-	echo "${*}" | sed s/\</\\n\</g | sed s/\>/\>\\n/g | sed "s/&apos;/\'/g" | sed 's/&nbsp;/ /g' | grep  "New in WebStorm ${PATTERN}" | sed s/\ /\\n/g | tail -1
-	# | grep "What&apos;s New in&nbsp;WebStorm&nbsp;" | sed 's/\;/\;'\\n'/g' | sed s/\</\\n\</g  )
-} # end _extract_version
-
-_fedora__64() {
-	raise_to_sudo_and_user_home
-	[ $? -gt 0 ] && failed to raise_to_sudo_and_user_home
-	# [ $? -gt 0 ] && exit 1
-  enforce_variable_with_value USER_HOME "${USER_HOME}"
-  local FIRST_URL=https://discord.com/api/download?platform=linux&format=tar.gz
-  enforce_variable_with_value FIRST_URL "${FIRST_URL}"
-  local CURLOUTPUT="$(curl -IL https://discord.com/api/download/ptb?platform=linux&format=tar.gz 2>/dev/null)"
-  # local CURLOUTPUT="$(curl -IL https://discord.com/api/download\?platform\=linux\&format\=tar.gz 2>/dev/null)"
-  enforce_variable_with_value CURLOUTPUT "${CURLOUTPUT}"
-  local TARGET_URL="$(echo "${TARGET_URL}" | grep "location:" | cut -d: -f2-)"
-  enforce_variable_with_value TARGET_URL "${TARGET_URL}"
-  local CODENAME=$(basename "${TARGET_URL}")
-  enforce_variable_with_value CODENAME "${CODENAME}"
-  local DOWNLOADFOLDER="${USER_HOME}/Downloads"
-  enforce_variable_with_value DOWNLOADFOLDER "${DOWNLOADFOLDER}"
-  _do_not_downloadtwice "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}"
-  _install_rpm "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}" 0
 } # end _fedora__64
+
+
+
+ #--------/\/\/\/\-- install_discord.bash -- Custom code-/\/\/\/\-------
+
 
 _main() {
   determine_os_and_fire_action
@@ -147,4 +259,5 @@ _main() {
 
 _main
 
-echo ":)"
+echo "🥦"
+exit 0
