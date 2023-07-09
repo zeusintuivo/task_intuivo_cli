245ea8a60998fd0c815b10fceaa4805508f381ce
zeusintuivo <zeus@intuivo.com>
2023-07-09 14:33:11 +0200

    added certbot docker

--- install_certbot.bash
+++ install_certbot.bash
@@ -233,13 +233,151 @@ directory_exists_with_spaces "${USER_HOME}"
 
 
 
- #--------\/\/\/\/-- Work here below \/, test, and transfer to tasks_templates/certbot having a working version -\/\/\/\/-------
+ #---------/\/\/\-- tasks_base/sudoer.bash -------------/\/\/\--------
 
 
 
 
 
- #--------/\/\/\/\-- Work here above /\, test, and transfer to tasks_templates/certbot having a working version -/\/\/\/\-------
+ #--------\/\/\/\/-- tasks_templates_sudo/certbot …install_certbot.bash” -- Custom code -\/\/\/\/-------
+
+
+#!/usr/bin/bash
+
+_debian_flavor_install() {
+  enforce_variable_with_value USER_HOME "${USER_HOME}"
+  if (
+    install_requirements "linux" "
+      certbot 
+      python3-certbot-apache
+      python3-certbot-nginx
+  "
+  ); then 
+    {
+      apt install certbot -y
+      apt install python3-certbot-apache -y
+      apt install python3-certbot-nginx -y
+    }
+  fi
+  verify_is_installed "
+      certbot 
+      python3-certbot-apache
+      python3-certbot-nginx
+  "
+  certbot --nginx --register-unsafely-without-email
+  certbot certonly --standalone --preferred-challenges
+  # local PB_VERSION=0.16.7
+  # local CODENAME="pocketbase_${PB_VERSION}_linux_amd64.zip"
+  # local TARGET_URL="https://github.com/pocketbase/pocketbase/releases/download/v${PB_VERSION}/${CODENAME}"
+  # local DOWNLOADFOLDER="$(_find_downloads_folder)"
+  # enforce_variable_with_value DOWNLOADFOLDER "${DOWNLOADFOLDER}"
+  # directory_exists_with_spaces "${DOWNLOADFOLDER}"
+  # cd "${DOWNLOADFOLDER}"
+  # _do_not_downloadtwice "${TARGET_URL}" "${DOWNLOADFOLDER}"  "${CODENAME}"
+  # # unzip "${DOWNLOADFOLDER}/${CODENAME}" -d $HOME/pb/
+  # local UNZIPDIR="${USER_HOME}/_/software"
+  # mkdir -p "${UNZIPDIR}"
+  # _unzip "${DOWNLOADFOLDER}" "${UNZIPDIR}" "${CODENAME}"
+  # local PATHTOPOCKETBASE="${UNZIPDIR}/pocketbase"
+  # local THISIP=$(myip)
+
+} # end _debian_flavor_install
+
+_redhat_flavor_install() {
+  echo "_redhat_flavor_install Procedure not yet implemented. I don't know what to do."
+} # end _redhat_flavor_install
+
+_arch_flavor_install() {
+  echo "_arch_flavor_install Procedure not yet implemented. I don't know what to do."
+} # end _readhat_flavor_install
+
+_arch__32() {
+  _arch_flavor_install
+} # end _arch__32
+
+_arch__64() {
+  _arch_flavor_install
+} # end _arch__64
+
+_centos__32() {
+  _redhat_flavor_install
+} # end _centos__32
+
+_centos__64() {
+  _redhat_flavor_install
+} # end _centos__64
+
+_debian__32() {
+  _debian_flavor_install
+} # end _debian__32
+
+_debian__64() {
+  _debian_flavor_install
+} # end _debian__64
+
+_fedora__32() {
+  _redhat_flavor_install
+} # end _fedora__32
+
+_fedora__64() {
+  _redhat_flavor_install
+} # end _fedora__64
+
+_gentoo__32() {
+  _redhat_flavor_install
+} # end _gentoo__32
+
+_gentoo__64() {
+  _redhat_flavor_install
+} # end _gentoo__64
+
+_madriva__32() {
+  _redhat_flavor_install
+} # end _madriva__32
+
+_madriva__64() {
+  _redhat_flavor_install
+} # end _madriva__64
+
+_suse__32() {
+  _redhat_flavor_install
+} # end _suse__32
+
+_suse__64() {
+  _redhat_flavor_install
+} # end _suse__64
+
+_ubuntu__32() {
+  _debian_flavor_install
+} # end _ubuntu__32
+
+_ubuntu__64() {
+  _debian_flavor_install
+} # end _ubuntu__64
+
+_darwin__64() {
+  echo "_darwin__64 Procedure not yet implemented. I don't know what to do."
+} # end _darwin__64
+
+_darwin__arm64() {
+  echo "_darwin__arm64 Procedure not yet implemented. I don't know what to do."
+} # end _darwin__arm64
+
+_tar() {
+  echo "_tar Procedure not yet implemented. I don't know what to do."
+} # end tar
+
+_windows__64() {
+  echo "_windows__64 Procedure not yet implemented. I don't know what to do."
+} # end _windows__64
+
+_windows__32() {
+  echo "_windows__32 Procedure not yet implemented. I don't know what to do."
+} # end _windows__32
+
+
+
+ #--------/\/\/\/\-- tasks_templates_sudo/certbot …install_certbot.bash” -- Custom code-/\/\/\/\-------
 
 
 _main() {
