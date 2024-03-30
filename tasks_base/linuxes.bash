63a7937221b37b6eed4ba6874ffded05399df5f4
zeusintuivo <zeus@intuivo.com>
2024-03-30 14:00:33 +0200

    added brew install ubuntu depth 12

--- tasks_base/linuxes.bash
+++ tasks_base/linuxes.bash
@@ -140,6 +140,11 @@ _ubuntu__aarch64() {
   _debian_flavor_install
 } # end _ubuntu__aarch64
 
+_ubuntu_22_aarch64() {
+  trap  '_trap_on_error $0 "${?}" LINENO BASH_LINENO FUNCNAME BASH_COMMAND $FUNCNAME $BASH_LINENO $LINENO   $BASH_COMMAND'  ERR
+  _debian_flavor_install
+} # end _ubuntu_22_aarch64
+
 _darwin__64() {
   trap  '_trap_on_error $0 "${?}" LINENO BASH_LINENO FUNCNAME BASH_COMMAND $FUNCNAME $BASH_LINENO $LINENO   $BASH_COMMAND'  ERR
   echo "_darwin__64 Procedure not yet implemented. I don't know what to do."
