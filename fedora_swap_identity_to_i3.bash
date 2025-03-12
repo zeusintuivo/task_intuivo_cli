44566f9e249db4165306ded53e7ba629fb7b4546
zeusintuivo <zeus@intuivo.com>
2025-03-12 22:16:16 +0100

    addded swap identities fedora

---
+++ fedora_swap_identity_to_i3.bash
@@ -0,0 +1,1 @@
+sudo dnf swap fedora-release-identity-workstation  fedora-release-identity-i3 --allowerasing
--- fedora_upgrade_to_give_number_here.bash
+++ fedora_upgrade_to_give_number_here.bash
@@ -3,9 +3,9 @@
 function main(){
 	local -i target="${1-}"
 	[[ -z "${1-}" ]] && echo -e "error give me number version 45 46 47 etc " && exit 1
-  sudo dnf upgrade --refresh -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+  sudo dnf upgrade --refresh  --releasever=${target}  -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular --disable-repo=rpmfusion-free --disable-repo=rpmfusion-free-updates-testing --disable-repo=rpmfusion-nonfree --disable-repo=rpmfusion-nonfree-nvidia-driver --disable-repo=rpmfusion-nonfree-updates-testing --disable-repo=scootersoftware --disable-repo=sublime-text --disable-repo=fedora-cisco-openh264 --disable-repo=keybase --disable-repo=nordvpn --disable-repo=nordvpn-noarch --disable-repo=cloudfoundry-cli --disable-repo=adoptium-temurin-java-repository --disable-repo=1password --disable-repo=cuda-fedora37-x86_64 --disable-repo=cuda-fedora39-x86_64
 
-  sudo dnf distro-sync -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+  sudo dnf distro-sync  --releasever=${target}  -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular --disable-repo=rpmfusion-free --disable-repo=rpmfusion-free-updates-testing --disable-repo=rpmfusion-nonfree --disable-repo=rpmfusion-nonfree-nvidia-driver --disable-repo=rpmfusion-nonfree-updates-testing --disable-repo=scootersoftware --disable-repo=sublime-text --disable-repo=fedora-cisco-openh264 --disable-repo=keybase --disable-repo=nordvpn --disable-repo=nordvpn-noarch --disable-repo=cloudfoundry-cli --disable-repo=adoptium-temurin-java-repository --disable-repo=1password --disable-repo=cuda-fedora37-x86_64 --disable-repo=cuda-fedora39-x86_64
 
 	sudo dnf install symlinks -y
 	sudo symlinks -r /usr | grep dangling
@@ -14,9 +14,9 @@ function main(){
 	sudo dnf remove golang-github-cespare-xxhash-devel -y
 	sudo dnf remove compat-golang-github-chzyer-readline-devel -y
 	sudo dnf remove bazel4 -y
-	sudo dnf system-upgrade download --releasever=${target} -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+	sudo dnf system-upgrade download --releasever=${target}  -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular --disable-repo=rpmfusion-free --disable-repo=rpmfusion-free-updates-testing --disable-repo=rpmfusion-nonfree --disable-repo=rpmfusion-nonfree-nvidia-driver --disable-repo=rpmfusion-nonfree-updates-testing --disable-repo=scootersoftware --disable-repo=sublime-text --disable-repo=fedora-cisco-openh264 --disable-repo=keybase --disable-repo=nordvpn --disable-repo=nordvpn-noarch --disable-repo=cloudfoundry-cli --disable-repo=adoptium-temurin-java-repository --disable-repo=1password --disable-repo=cuda-fedora37-x86_64 --disable-repo=cuda-fedora39-x86_64
   sudo dnf install https://rpms.remirepo.net/fedora/remi-release-${target}.rpm -y
-	sudo dnf system-upgrade reboot -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+	sudo dnf system-upgrade reboot -y
 
 } # end main
 
