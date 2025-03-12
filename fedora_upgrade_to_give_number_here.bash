25b6301b3e0d21bc5160e082e5caf3debb60792a
zeusintuivo <zeus@intuivo.com>
2025-03-12 12:03:48 +0100

    added more conditions more robust install fedora script

--- fedora_upgrade_to_give_number_here.bash
+++ fedora_upgrade_to_give_number_here.bash
@@ -3,8 +3,10 @@
 function main(){
 	local -i target="${1-}"
 	[[ -z "${1-}" ]] && echo -e "error give me number version 45 46 47 etc " && exit 1
-  sudo dnf upgrade --refresh -y
-  sudo dnf distro-sync -y
+  sudo dnf upgrade --refresh -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+
+  sudo dnf distro-sync -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+
 	sudo dnf install symlinks -y
 	sudo symlinks -r /usr | grep dangling
 	sudo symlinks -r -d /usr
@@ -12,8 +14,10 @@ function main(){
 	sudo dnf remove golang-github-cespare-xxhash-devel -y
 	sudo dnf remove compat-golang-github-chzyer-readline-devel -y
 	sudo dnf remove bazel4 -y
-	sudo dnf system-upgrade download --releasever=${target} -y
-	sudo dnf system-upgrade reboot -y
+	sudo dnf system-upgrade download --releasever=${target} -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+  sudo dnf install https://rpms.remirepo.net/fedora/remi-release-${target}.rpm -y
+	sudo dnf system-upgrade reboot -y --allowerasing --nobest --disable-repo=remi --disable-repo=remi-modular
+
 } # end main
 
 main "${1}"
